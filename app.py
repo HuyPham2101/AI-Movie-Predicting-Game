19cec2c774db1a49b55a5eafb90bc2e10bf8b458
Hieu Le <lehieu.jo@gmail.com>
2021-01-21 23:33:53 +0100

    chore(SPARQL) : implement sparql

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-# from flaskwebgui import FlaskUI
+
 from string import Template
 from SPARQLWrapper import SPARQLWrapper, JSON
 import rdflib
@@ -13,16 +13,32 @@ PREFIX : <https://www.themoviedb.org/kaggle-export/>
 SELECT ?actor
 WHERE{?m a :Movie ; :title "$title" ;  :cast/:name?actor.}
 """
+info_query = """
+PREFIX : <https://www.themoviedb.org/kaggle-export/>
+SELECT ?original_lang ?overview ?release_date ?status ?runtime ?budget ?revenue
+    WHERE {
+    ?m a :Movie;
+        :title "$title" ;
+        :original_language ?original_lang ;
+        :release_date ?release_date ;
+        :status ?status ;
+        :runtime ?runtime ;
+        :overview ?overview ;
+        :budget ?budget ;
+        :revenue ?revenue;
+    }
+
+"""
 
 app = Flask(__name__)
 
 
 def query(title):
-    query_string = Template(actor_query).substitute(title=title)
+    query_string = Template(info_query).substitute(title=title)
     sparql.setQuery(query_string)
     sparql.setReturnFormat(JSON)
     results_dict = sparql.query().convert()
-    results = [row['actor']['value'] for row in results_dict['results']['bindings']]
+    results = [obj for obj in results_dict['results']['bindings']]
     return results
 
 
@@ -30,9 +46,9 @@ def query(title):
 def index():
     if request.method == 'POST':
         title = request.form['title']
-        return render_template('index.html', actors=[])
+        return render_template('index.html', info=query(title))
     else:
-        return render_template('index.html', actors=[])
+        return render_template('index.html', info=[])
 
 
 if __name__ == '__main__':
